078eee58d5dfb981a4fb1a6034ac81c4a57593a2
Travis CI <travis@travis-ci.org>
2020-01-28 09:31:15 +0000

    Fix compilation issue

---
+++ src/main/scala/com/example/persistence/postgres/PostgresError.scala
@@ -0,0 +1,24 @@
+package com.example.persistence.postgres
+
+import com.example.util.error.ThrowableSelect
+import com.example.util.instances.render._
+import io.odin.meta.Render
+
+@scalaz.deriving(Render)
+sealed trait PostgresError
+
+object PostgresError {
+
+  final case class UnavailableConnection(cause: Throwable) extends PostgresError
+
+  final case class ConnectionAttemptTimeout(message: String) extends PostgresError
+
+  def unavailableConnection(cause: Throwable): PostgresError   = UnavailableConnection(cause)
+  def connectionAttemptTimeout(message: String): PostgresError = ConnectionAttemptTimeout(message)
+
+  implicit val postgresErrorThrowableSelect: ThrowableSelect[PostgresError] = {
+    case _: ConnectionAttemptTimeout  => None
+    case UnavailableConnection(cause) => Option(cause)
+  }
+
+}
---
+++ src/main/scala/com/example/util/execution/Retry.scala
@@ -0,0 +1,41 @@
+package com.example.util.execution
+
+import cats.Applicative
+import com.example.util.error.ThrowableSelect
+import com.example.util.instances.render._
+import eu.timepit.refined.auto._
+import eu.timepit.refined.types.numeric.NonNegInt
+import io.circe.Decoder
+import io.circe.config.syntax.durationDecoder
+import io.circe.refined._
+import io.odin.Logger
+import io.odin.meta.Render
+import io.odin.syntax._
+import retry.{RetryDetails, RetryPolicies, RetryPolicy}
+
+import scala.concurrent.duration.FiniteDuration
+
+object Retry {
+
+  def makePolicy[F[_]: Applicative](retryPolicy: Policy): RetryPolicy[F] = {
+    val policy = RetryPolicies
+      .constantDelay(retryPolicy.delay)
+      .join(RetryPolicies.limitRetries(retryPolicy.retries))
+
+    RetryPolicies.limitRetriesByCumulativeDelay(retryPolicy.timeout, policy)
+  }
+
+  def logErrors[F[_]: Applicative, E: Render: ThrowableSelect](logger: Logger[F]): (E, RetryDetails) => F[Unit] =
+    (error, details) => {
+      ThrowableSelect[E].select(error) match {
+        case Some(cause) => logger.error(render"Retry policy. Error $error. $details", cause)
+        case None        => logger.error(render"Retry policy. Error $error. $details")
+      }
+    }
+
+  @scalaz.deriving(Decoder, Render)
+  final case class Policy(retries: NonNegInt, delay: FiniteDuration, timeout: FiniteDuration)
+
+  implicit val renderRetryDetails: Render[RetryDetails] = io.odin.extras.derivation.render.derive
+
+}

c6c2e7daa7b2b97043e75ceadf0faf8105cc6ef1
Maksim Ochenashko <maxochenashko@gmail.com>
2020-05-14 10:48:17 +0300

    Update flyway to 6.4.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Versions {
   val monix               = "3.2.1"
   val sup                 = "0.7.0"
   val doobie              = "0.9.0"
-  val flyway              = "6.4.1"
+  val flyway              = "6.4.2"
   val newtype             = "0.4.4"
   val refined             = "0.9.14"
   val magnolia            = "0.16.0"

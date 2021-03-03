eebdd41ddeb12838cf341aa544f65e26b96e06b6
Travis CI <travis@travis-ci.org>
2021-03-03 16:36:29 +0000

    update versions of monix and cats-effect

---
+++ src/main/scala/com/example/util/logging/package.scala
@@ -0,0 +1,13 @@
+package com.example.util
+
+import cats.mtl.Local
+
+package object logging {
+
+  type TraceProvider[F[_]] = Local[F, TraceId]
+
+  object TraceProvider {
+    def apply[F[_]](implicit instance: TraceProvider[F]): TraceProvider[F] = instance
+  }
+
+}
